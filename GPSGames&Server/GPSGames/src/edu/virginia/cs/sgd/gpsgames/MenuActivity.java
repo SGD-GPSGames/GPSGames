ab3ce8194372e2dee4177b5ab1b6adfcd9a43cfd
Zack Reilly <ilr9xy@virginia.edu>
2013-03-03 17:17:39 -0500

    updated menu activity

--- GPSGames&Server/GPSGames/src/edu/virginia/cs/sgd/gpsgames/MenuActivity.java
+++ GPSGames&Server/GPSGames/src/edu/virginia/cs/sgd/gpsgames/MenuActivity.java
@@ -1,9 +1,13 @@
 package edu.virginia.cs.sgd.gpsgames;
 
 import edu.virginia.cs.sgd.gpsgames.util.Constants;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -27,6 +31,14 @@ public class MenuActivity extends FragmentActivity{
 		
 		populateGameList();
 		
+		create.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				moveToCreateActivity();	
+			}
+		});
+		
 		
 	}
 
@@ -35,12 +47,21 @@ public class MenuActivity extends FragmentActivity{
 		welcome = (TextView) findViewById(R.id.menu_welcome);
 		create = (Button) findViewById(R.id.menu_create);
 		join = (Button) findViewById(R.id.menu_join);
-		gameList = (ListView)findViewById(R.id.menu_gameList);
-		
+		gameList = (ListView)findViewById(R.id.menu_gameList);	
+	}
+	
+	public void moveToCreateActivity() {
+		//REPLACE MenuActivity.clss BELOW WITH CLASS YOU WANT TO GO TO
+		Intent mainActivity = new Intent(this, MenuActivity.class);
+		startActivity(mainActivity);
 	}
 	
 	public void populateGameList() {
-		//gameList.add
+		String[]  games = { "Game 1", "Game 2", "Game 3", "Game 4", "Game 5", "Game 6", "Game 7", "Game 8"};
+		ArrayAdapter adapter = new ArrayAdapter<String>(this, 
+		        android.R.layout.simple_list_item_1, games);
+		
+		gameList.setAdapter(adapter);
 	}
 	
 	@Override
